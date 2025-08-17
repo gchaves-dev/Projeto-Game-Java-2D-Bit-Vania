7857ca9d35c7bdda2413439bd6cd16f98a277532
GustavoDev <gchaves427@gmail.com>
2025-08-16 21:14:29 -0300

    render test

--- game_bitvania_001_screen/src/main/GamePanel.java
+++ game_bitvania_001_screen/src/main/GamePanel.java
@@ -7,7 +7,7 @@ import java.awt.Graphics2D;
 
 import javax.swing.JPanel;
 
-public class GamePanel extends JPanel{
+public class GamePanel extends JPanel implements Runnable{
 	
 	private static final long serialVersionUID = 1L;
 	
@@ -29,7 +29,49 @@ public class GamePanel extends JPanel{
 		this.setBackground(Color.black);
 		this.setDoubleBuffered(true);
 		
-	}	
+	}
 	
+	public void startGameThread() {
+		
+		gameThread = new Thread(this);
+		gameThread.start();
+		
+	}
+
+	@Override
+	public void run() {
+		
+		while(gameThread != null) {
+			
+			//System.out.println("The game loop is running");
+			
+			// 1 UPDATE: update information auch as character positions
+			update();
+			
+			// 2 DRAW: draw the screen with the updated information
+			repaint();
+			
+		}
+		
+	}
+	
+	public void update() {
+		
+	}
+	
+	public void paintComponent(Graphics g) {
+		
+		super.paintComponent(g);
+		
+		Graphics2D g2 = (Graphics2D)g;
+		
+		g2.setColor(Color.white);
+		
+		g2.fillRect(100, 100, tileSize, tileSize);
+		
+		g2.dispose();
+		
+		
+	}
 
 }
