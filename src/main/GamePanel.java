abfed55aed7ffcc11207758a7b616dacfd9950c8
GustavoDev <gchaves427@gmail.com>
2025-09-06 20:05:18 -0300

    distributing grass on the screen

--- src/main/GamePanel.java
+++ src/main/GamePanel.java
@@ -19,8 +19,8 @@ public class GamePanel extends JPanel implements Runnable {
 	final int scale = 3;
 
 	public final int tileSize = originalTileSize * scale; // 48x48 pixels
-	final int maxScreenCol = 16;
-	final int maxScreenRow = 12;
+	public final int maxScreenCol = 16;
+	public final int maxScreenRow = 12;
 	final int screenWidth = tileSize * maxScreenCol; // 768 pixels
 	final int screenHeight = tileSize * maxScreenRow; // 576 pixels
 
