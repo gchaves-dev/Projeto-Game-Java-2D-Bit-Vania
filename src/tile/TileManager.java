34401a226c9ebb66a6dc00401a01ac17e8eef049
GustavoDev <gchaves427@gmail.com>
2025-09-09 18:22:23 -0300

    limiting render by camera

--- src/tile/TileManager.java
+++ src/tile/TileManager.java
@@ -70,7 +70,7 @@ public class TileManager {
 
 				String line = br.readLine();
 
-				while (col < gp.maxWorldCol) {
+				while (col < gp.maxWorldRow) {
 
 					String numbers[] = line.split(" ");
 
@@ -105,8 +105,16 @@ public class TileManager {
 			int worldY = worldRow * gp.tileSize;
 			int screenX = worldX - gp.player.worldX + gp.player.screenX;
 			int screenY = worldY - gp.player.worldY + gp.player.screenY;
+			
+			if(worldX + gp.tileSize > gp.player.worldX - gp.player.screenX && 
+				worldX - gp.tileSize < gp.player.worldX + gp.player.screenX &&
+				worldY + gp.tileSize > gp.player.worldY - gp.player.screenY &&
+				worldY - gp.tileSize < gp.player.worldY + gp.player.screenY) {
+				
+				g2.drawImage (tile[tileNum] .image, screenX, screenY, gp.tileSize, gp.tileSize, null) ;
+			}
 	
-			g2.drawImage (tile[tileNum] .image, screenX, screenY, gp.tileSize, gp.tileSize, null) ;
+			
 			worldCol++;
 	
 			if(worldCol == gp.maxWorldCol) {
